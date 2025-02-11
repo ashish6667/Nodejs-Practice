2764cec81728449934308b585eb4054b4f75f486
ashish6667 <ay1060045@gmail.com>
2025-02-11 15:52:50 +0530

    Schema design

--- server.js
+++ server.js
@@ -1,25 +1,49 @@
 const express = require('express')
-const app = express()
+const app = express();
+const db = require('./db');
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json()); // req.body
+
+
+const Person = require('./models/Person');
+const MenuItem = require('./models/MenuItem');
 
 app.get('/', function (req, res) {
   res.send('Welcome to my hotel... How i can help you')
 })
 
-app.get('/chicken', (req, res)=>{
-    res.send('sure sir, i would love to serve chicken')
+// POST route to add a person
+app.post('/person', async (req, res)=>{
+  try{
+    const data = req.body // Assuming the request body contains the person data
+
+    // Create a new Person document using the Mongoose model
+   const newPerson = new Person(data);
+
+   // Save the new person to the database
+    const response = await newPerson.save();
+    console.log('data saved');
+    res.status(200).json(response);
+  }
+  catch(err){
+    console.log(err);
+    res.status(500).json({error: 'Internal Server Error'});
+  }   
+  
 })
 
-app.get('/idli', (req, res)=>{
-    var customized_idli = {
-        name: 'rava idli',
-        size: '10cm daimeter',
-        is_sambhar: true
-    }
-    res.send(customized_idli)
-})
 
-app.post('/items', (req, res)=>{
-    res.send('data saved');
+// GET method to get the person
+app.get('/person', async (req, res)=>{
+  try{
+     const data = await Person.find();
+     console.log('data fetched');
+     res.status(200).json(data);
+  }catch(err){
+    console.log(err);
+    res.status(500).json({error: 'Internal Server Error'});
+  }
 })
 
 app.listen(3000, ()=>{
